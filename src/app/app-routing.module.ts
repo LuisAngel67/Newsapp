afcd57b9f06b0f276f25d735fb70615795df2d81
Luis Zabaleta <luisangelzabaleta67@gmail.com>
2025-08-30 14:04:02 -0500

    Cree la pagina login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
 
 ];
 
